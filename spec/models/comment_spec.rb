5bef8844c899000718d37f0e90df595ef4dd4a3c
MrFrozeN <bartekstafiej2@gmail.com>
2020-09-08 18:53:38 +0200

    refactoring spec models of post and comment - added association

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,6 +5,5 @@ RSpec.describe Comment, type: :model do
   it { should validate_presence_of :content }
   it { should belong_to :post }
   it { should validate_length_of :content }
-  it { should belong_to :user }
 
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,5 +6,4 @@ RSpec.describe Post, type: :model do
   it { should validate_presence_of :content }
   it { should validate_length_of :title }
   it { should validate_length_of :content }
-  it { should belong_to :user }
 end
